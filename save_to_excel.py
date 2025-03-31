05d4ceae40f1d8f956d7e04f5589629dde15a0aa
scs20913@naver.com <scs20913@naver.com>
2025-03-31 19:40:22 +0900

    Add files via upload

--- save_to_excel.py
+++ save_to_excel.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from io import BytesIO
 
-def save_to_excel(df, suggestions):
+def save_analysis_and_suggestions(df, suggestions):
     output = BytesIO()
-    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+    with pd.ExcelWriter(output, engine="openpyxl") as writer:
         df.to_excel(writer, index=False, sheet_name="키워드 분석")
         if suggestions:
             pd.DataFrame({"추천 상품명": suggestions}).to_excel(writer, index=False, sheet_name="추천 상품명")
